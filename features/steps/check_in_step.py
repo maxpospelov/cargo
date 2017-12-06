a20cb461303405cc0d972c4db4fc6337650fda92
maxpospelov <maxpospelov1982@gmail.com>
2017-12-06 18:18:10 +0500

    Init

---
+++ features/steps/check_in_step.py
@@ -0,0 +1,20 @@
+from pyshould import *
+from behave import given, when, then
+
+
+URL_PROJECT = 'http://localhost:8000'
+
+
+@given('Адрес веб страницы')
+def step_impl(context):
+    pass
+
+
+@when('Оператор открывает страницу')
+def step_impl(context):
+    pass
+
+
+@then('Тогда видеть титульник')
+def step_impl(context):
+    pass
