82c461bbbfbed333a0b807851004502c8560d883
maxpospelov <maxpospelov1982@gmail.com>
2017-12-14 19:02:35 +0500

    feature(route form): ci test.

--- features/steps/check_in_step.py
+++ features/steps/check_in_step.py
@@ -1,5 +1,6 @@
 from pyshould import *
 from behave import given, when, then
+from capybara.dsl import *
 
 
 URL_PROJECT = 'http://localhost:8000'
@@ -7,14 +8,31 @@ URL_PROJECT = 'http://localhost:8000'
 
 @given('Адрес веб страницы')
 def step_impl(context):
-    pass
+    visit(URL_PROJECT)
 
 
 @when('Оператор открывает страницу')
 def step_impl(context):
-    pass
+    context.has_title = page.has_title("GARGO")
 
 
 @then('Тогда видеть титульник')
 def step_impl(context):
-    pass
+    assert context.has_title, True
+
+
+@given('Оператор на странице ввода')
+def step_impl(context):
+    visit(URL_PROJECT)
+
+
+@when('Опертор ввел данные')
+def step_impl(context):
+    page.fill_in("Driver", value="Driver A")
+    page.click_button("Send")
+
+
+@then('На странице появляется таблица с данными')
+def step_impl(context):
+    page.visit("/route")
+    assert page.has_text("Driver A"), True
